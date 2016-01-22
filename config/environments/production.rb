ccfd0df8d58d038d6925bddf25840f05dc5e909e
jennaleeb <jennaleeblumenthal@gmail.com>
2016-01-22 09:40:09 -0500

    configure devise for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set up devise for production environment
+  config.assets.initialize_on_precompile = false
 end
